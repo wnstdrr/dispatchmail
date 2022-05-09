19e4d60536f9803c6a161ebf8d9e7d022d2656fa
wnstdrr <xandertedder7@gmail.com>
2022-05-09 07:17:07 -0400

    fixed SendMail, added mail, dict change

--- mailing.py
+++ mailing.py
@@ -20,16 +20,35 @@ class EmailConst(object):
 
     def __str__(self):
         m = self.__dict__()
-        return namedtuple(
+        return str(namedtuple(
             "EmailConst", ["Subject", "To", "From", "Content"])(
                 m.get("Subject"), m.get("To"), m.get("From"), m.get("Subject"
                 )
-            )
-    
-    def SendMail(self, Login: tuple, Content: dict = None, Mailing: str = "localhost") -> dict:
-        '''Send mail via mailing server, use (smtp.gmail.com, 587) for gmail hosting.'''
+            ))
 
-        if (Content == None):
+    @property
+    def mail(self):
+        if (type(self.To) == type([]) and len(self.To) >= 2):
+            to_string = str(", ".join(self.To))
+        else:
+            to_string = self.To
+        
+        return f"""\
+From: {self.From}
+To: {to_string}
+Subject: {self.Subject}
+
+{self.Content}"""
+
+    def SendMail(self, Login: tuple, Content: dict = None, Mailing: str = "localhost") -> dict:
+        '''Send mail via mailing server, use (smtp.gmail.com, 587) for gmail hosting.
+           
+           NOTE: starting May 30, 2022, Google will no longer support the use of third-party apps or devices
+           and the smtp.gmail.com server will be broken
+        '''
+        
+        username, passwd = Login[0], Login[1]
+        if (Content is None):
             Content = self.__dict__()
 
         context = ssl.create_default_context()
@@ -38,7 +57,7 @@ class EmailConst(object):
             smtplib_server.starttls(context=context)
             smtplib_server.ehlo()
 
-            smtplib_server.login(Login[0], Login[1])
-            smtplib_server.sendmail(Login[0], Content.get("To"), Content.get("Content"))
+            smtplib_server.login(username, passwd)
+            smtplib_server.sendmail(username, Content.get("To"), self.mail)
 
         return self.__dict__()
