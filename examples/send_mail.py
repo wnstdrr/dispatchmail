824e4940e5c1c836f89f41a1535c911a24a1795b
wnstdrr <xandertedder7@gmail.com>
2022-05-09 07:34:40 -0400

    example updated

---
+++ examples/send_mail.py
@@ -0,0 +1,8 @@
+from mailing import EmailConst as Mail
+
+def create_mail(login=("username", "password"), MailServer=("smtp.gmail.com", 587), From="example@mailserver.com"):
+    mail = Mail("Automated Subject", ["user1@mailserver.com", "user2@mailserver.com"], From, "Hello, from mailing!")
+    mail.SendMail(login, Mailing=MailServer)
+    return mail.__dict__()
+
+create_mail()
