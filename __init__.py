a683798a1c9ffdd83ba4abbbed90e8efb5e3cb2b
wnstdrr <xandertedder7@gmail.com>
2022-05-10 19:36:15 -0400

    version, license magics

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 from mailing.mailing import EmailConst, LoadMail
 
 __author__ = "Xander Tedder (xandertedder7@gmail.com)"
+__version__ = "0.0.1"
+__license__ = "Apache 2.0"
+
 __name__ = "mailing"
 
 __all__ = ["EmailConst", "LoadMail"]
