177a3e80475fa5b3b2a50d5531c9846ec83fd0ec
wnstdrr <xandertedder7@gmail.com>
2022-05-10 19:32:44 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mailing",
+    version="0.0.1",
+    license="Apache 2.0",
+    author="Xander Tedder",
+    author_email="xandertedder7@gmail.com",
+    url="https://github.com/wnstdrr/mailing",
+    keywords="smtp json ini mailing email",
+    description="Light, simple, mailing api written in Python",
+    packages=find_packages()
+)
