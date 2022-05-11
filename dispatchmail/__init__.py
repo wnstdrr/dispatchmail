8e6dc6d41a5a5ebc257ca954ba37b83a31323efb
wnstdrr <xandertedder7@gmail.com>
2022-05-10 21:14:14 -0400

    version update

--- dispatchmail/__init__.py
+++ dispatchmail/__init__.py
@@ -1,7 +1,7 @@
 from dispatchmail.dispatchmail import EmailConst, LoadMail
 
 __author__ = "Xander Tedder (xandertedder7@gmail.com)"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __license__ = "Apache 2.0"
 
 __name__ = "dispatchmail"
