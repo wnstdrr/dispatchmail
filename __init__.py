17cb65095ca0f082bf10bd834fb868157b1f0dc7
wnstdrr <xandertedder7@gmail.com>
2022-05-10 20:30:10 -0400

    updated PyPi setup

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from dispatchmail.mailing import EmailConst, LoadMail
+from dispatchmail.dispatchmail import EmailConst, LoadMail
 
 __author__ = "Xander Tedder (xandertedder7@gmail.com)"
 __version__ = "0.0.1"
